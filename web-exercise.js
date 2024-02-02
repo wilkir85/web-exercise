18ee5ba0d23434b20a6a1d7aeef17c61fa8be68c
wilkir <wilkir85@gmail.com>
2024-02-02 18:30:31 -0500

    commit

---
+++ web-exercise.js
@@ -0,0 +1,21 @@
+// 1. hyper text transfer protocol dictates how data is sent to a server.
+
+// 2. A URL is a universal resource locator consisting of a protocall, host name, port, resource, and query. Used in the searchbar to locate sites.
+
+// 3. DNS is a domain name system which translates thehost name to the ip address.
+
+// 4. A query string provides extra information like search terms, info from, forms, etc.
+
+// 5. The query string allows you to pass key-value pairs into the URL.
+
+// 6. GET, get data from the server
+//    POST, send data to the server 
+
+// 7. An http is a request to a server which follows http protocalls.
+
+// 8. An http response is a response from a server follows the http protocol.
+
+// 9. Headers provide additional information about the request or the response.
+
+// 10.  Browser converts name to ip address with dns, browser makes request to ip address, server sends response, browse makes a dom and locates neede resources, 
+// browser makes http requests resources and receives responce from server
